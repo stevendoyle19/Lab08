2a483c8b1318f079f6c2409f1b62962d22846544
steve doyle <steven_doyle19@uri.edu>
2021-03-25 10:42:42 -0400

    added hiFacebook.py

---
+++ hiFacebook.py
@@ -0,0 +1,9 @@
+userName = input("Hello! Please enter your facebook username! ")
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "steven" and userPassword == "ClamCake!"):
+    print("Welcome to facebook!")
+
+else:
+    print("Wrong credentials, try again!")
