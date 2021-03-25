4cb178d6e1f4add75bf3c0739a2fa2c105c778b2
steve doyle <steven_doyle19@uri.edu>
2021-03-24 23:33:06 -0400

    added helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/CyBeerGansett/Desktop/helloWorld.py ============
+Hello World, my name is Steve Doyle and I love Python!
+>>> 
